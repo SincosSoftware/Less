128496255f1e10f643b71db6a43393effb259844
Torkil Sinkaberg Johnsen <torkil@24nettbutikk.no>
2013-07-23 16:24:28 +0200

    i18n

--- functions.php
+++ functions.php
@@ -1,5 +1,10 @@
 <?php
 
+add_action('after_setup_theme', 'my_theme_setup');
+function my_theme_setup(){
+    load_theme_textdomain('less', get_template_directory() . '/lang');
+}
+
 // Define the version as a constant so we can easily replace it throughout the theme
 define( 'LESS_VERSION', 1.1 );
 
@@ -12,7 +17,7 @@ add_theme_support( 'automatic-feed-links' );
 /*-----------------------------------------------------------------------------------*/
 /* register main menu
 /*-----------------------------------------------------------------------------------*/
-register_nav_menus( 
+register_nav_menus(
 	array(
 		'primary'	=>	__( 'Primary Menu', 'less' ),
 	)
@@ -22,16 +27,16 @@ register_nav_menus(
 /* Enque Styles and Scripts
 /*-----------------------------------------------------------------------------------*/
 
-function less_scripts()  { 
+function less_scripts()  {
 
 	// theme styles
 	wp_enqueue_style( 'less-style', get_template_directory_uri() . '/style.css', '10000', 'all' );
-			
+
 	// add fitvid
 	wp_enqueue_script( 'less-fitvid', get_template_directory_uri() . '/js/jquery.fitvids.js', array( 'jquery' ), LESS_VERSION, true );
-	
+
 	// add theme scripts
 	wp_enqueue_script( 'less', get_template_directory_uri() . '/js/theme.min.js', array(), LESS_VERSION, true );
-  
+
 }
 add_action( 'wp_enqueue_scripts', 'less_scripts' );
