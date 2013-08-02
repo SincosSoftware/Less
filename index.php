441145d33f680285c6eddb5a091e359176961b8f
Torkil Sinkaberg Johnsen <torkil@24nettbutikk.no>
2013-08-02 10:46:28 +0200

    Footer credits

--- index.php
+++ index.php
@@ -228,9 +228,7 @@
 <footer class="site-footer" role="contentinfo">
 	<div class="site-info container">
 		<?php do_action( 'break_credits' ); ?>
-		<a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress</a>
-		<span class="sep"> and </span>
-		<a href="http://lessmade.com/themes/less" rel="theme">LESS</a> by <a href="http://jarederickson.com" rel="designer">Jared Erickson</a>
+		Skrevet av teamet bak <a href="http://24nettbutikk.no">24Nettbutikk</a>
 	</div><!-- .site-info -->
 </footer><!-- #colophon .site-footer -->
 
